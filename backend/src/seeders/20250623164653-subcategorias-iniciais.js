2238ea262d01ab174ed376697919c2d8b6daa4b0
Rian-Noronha <riannoronha011@gmail.com>
2025-06-23 14:05:49 -0300

    feat(seed): pupulando com subcategorias

---
+++ backend/src/seeders/20250623164653-subcategorias-iniciais.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('subcategorias', [
+
+      {
+          nome: 'Bloqueio',
+          descricao: 'Subcategoria relacionada à categoria Cartão.',
+          categoria_id: 2,
+          createdAt: new Date(),
+          updatedAt: new Date()
+      },
+
+      {
+          nome: 'Empréstimo',
+          descricao: 'Subcategoria relacionada à categoria Cartão.',
+          categoria_id: 2,
+          createdAt: new Date(),
+          updatedAt: new Date()
+      },
+
+      {
+          nome: 'Data Base',
+          descricao: 'Subcategoria relacionada à categoria Cartão.',
+          categoria_id: 2,
+          createdAt: new Date(),
+          updatedAt: new Date()
+      },
+
+      {
+        nome: 'Chamados',
+        descricao: 'Subcategoria relacionada à categoria SAC.',
+        categoria_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+      return queryInterface.bulkDelete('subcategorias', null, {});
+  }
+
+};
