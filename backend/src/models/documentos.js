f8d93d1e51797e288256091a8a7fffe36e9181cc
Rian-Noronha <riannoronha011@gmail.com>
2025-06-23 16:01:29 -0300

    feat(model): aplicando a relação n:n entre documentos e palavras-chave

--- backend/src/models/documentos.js
+++ backend/src/models/documentos.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.belongsToMany(models.palavraschave, {
+        through: 'documentos_palavras_chave',
+        foreignKey: 'documentos_id',
+        as: 'palavrasChave'
+      });
     }
   }
   Documentos.init({
--- backend/src/models/palavraschave.js
+++ backend/src/models/palavraschave.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.belongsToMany(models.BaseSolucao, {
+        through: 'documentos_palavras_chave',
+        foreignKey: 'palavras_chave_id',
+        as: 'solucoes'
+      });
     }
   }
   PalavrasChave.init({
