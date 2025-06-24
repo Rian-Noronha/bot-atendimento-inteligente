5454af2397dbfba71fb6ace0805d4bf44fc5aa35
Rian-Noronha <riannoronha011@gmail.com>
2025-06-24 14:48:14 -0300

    feat(model): criação do model de chat_sessao

---
+++ backend/src/migrations/20250624173918-create-chat-sessao.js
@@ -0,0 +1,36 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('chat_sessoes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      usuario_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'usuarios',
+          key: 'id'
+        },
+
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('chat_sessoes');
+  }
+};
