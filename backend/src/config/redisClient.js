7f86657b1362e235d94da93fe3ca3e847809ae1b
Rian-Noronha <riannoronha011@gmail.com>
2025-07-14 21:57:11 -0300

    refactor(redis): ajustes na exportação

--- backend/src/config/redisClient.js
+++ backend/src/config/redisClient.js
@@ -1,7 +1,6 @@
-import { createClient } from 'redis';
+const { createClient } = require('redis');
 const redisClient = createClient();
 
-//  "escutas" para eventos de conexão.
 redisClient.on('connect', () => {
     console.log('Conectado ao Redis com sucesso!');
 });
@@ -10,10 +9,9 @@ redisClient.on('error', (err) => {
     console.error('Erro na conexão com o Redis:', err);
 });
 
-//auto-executável para garantir que ele tente conectar assim que o arquivo for carregado.
 (async () => {
     await redisClient.connect();
 })();
 
 
-export default redisClient;
+module.exports = redisClient;
