f94559454c4e00f9e1e1bdf8ff362a91d0fd136a
Rian-Noronha <riannoronha011@gmail.com>
2025-07-14 21:36:18 -0300

    feat(redis): configuração do servidor

---
+++ backend/src/config/redisClient.js
@@ -0,0 +1,19 @@
+import { createClient } from 'redis';
+const redisClient = createClient();
+
+//  "escutas" para eventos de conexão.
+redisClient.on('connect', () => {
+    console.log('Conectado ao Redis com sucesso!');
+});
+
+redisClient.on('error', (err) => {
+    console.error('❌ Erro na conexão com o Redis:', err);
+});
+
+//auto-executável para garantir que ele tente conectar assim que o arquivo for carregado.
+(async () => {
+    await redisClient.connect();
+})();
+
+
+export default redisClient;
